dd968539f20cfc2c5129f34c82f998ec29fd5f2c
Alison Vinicius <viniciussoares024@gmail.com>
2025-02-20 18:17:04 -0500

    inicializando back-end

---
+++ codeFit-api/src/main/java/com/example/codeFit_api/CodeFitApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeFit_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFitApiApplication.class, args);
+	}
+
+}
---
+++ codeFit-api/src/test/java/com/example/codeFit_api/CodeFitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeFit_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
