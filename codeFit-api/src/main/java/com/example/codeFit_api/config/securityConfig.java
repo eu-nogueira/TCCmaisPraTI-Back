1b4fdc9e8ad523556b182653c2f79a2f26aa5b4e
Alison Vinicius <viniciussoares024@gmail.com>
2025-02-24 19:11:57 -0500

    configurando cors

--- codeFit-api/src/main/java/com/example/codeFit_api/config/securityConfig.java
+++ codeFit-api/src/main/java/com/example/codeFit_api/config/securityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class securityConfig {
@@ -25,5 +28,18 @@ public class securityConfig {
                         .anyRequest().authenticated() // Exige autenticação para qualquer outra requisição não especificada acima.
                 )
                 .build(); // Constrói e retorna a configuração de segurança definida.
+    }
+
+    @Bean // Define que o método abaixo retornará um bean gerenciado pelo Spring
+    public CorsConfigurationSource corsConfigurationSource() { // Método que configura e retorna as definições de CORS
+        CorsConfiguration configuration = new CorsConfiguration(); // Cria uma nova instância de CorsConfiguration para definir as configurações de CORS
+        configuration.addAllowedOrigin("http://localhost:5173"); // Permite requisições de origem "http://localhost:5173"
+        configuration.addAllowedMethod("*"); // Permite todos os métodos HTTP (GET, POST, PUT, DELETE, etc.)
+        configuration.addAllowedHeader("*"); // Permite todos os cabeçalhos HTTP
+        configuration.setAllowCredentials(true); // Permite o envio de credenciais (cookies, cabeçalhos de autorização, etc.) nas requisições
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); // Cria uma nova instância para mapear as configurações de CORS com base em URLs
+        source.registerCorsConfiguration("/**", configuration); // Registra as configurações de CORS para todas as rotas da aplicação
+        return source; // Retorna a fonte de configuração CORS
     }
 }
